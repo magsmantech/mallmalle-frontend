20593d7054a0cae47f1b64b290ed8fbcb3a57197
Levan Madurashvili <55601009+maduraaa@users.noreply.github.com>
2022-05-18 15:38:16 +0400

    purchase history full responsive

--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -9,21 +9,41 @@ import styled from "styled-components";
 import { Badge } from "../../components/OrdersList";
 import { IoLocationSharp } from "react-icons/io5";
 import Link from "next/link";
+import Responsive from "../../config/Responsive";
 
 
-
+const SectionStyle = styled.section`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 100px;
+`;
 const ItemLabel = styled.span`
     color: var(--text-color);
     font-size: 1.8rem;
     font-family: 'helvetica';
     opacity: 0.5;
 `;
-
+const WrapperStyle = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 47px;
+    gap: 15px;
+        ${Responsive.tablet} {
+            flex-direction: column;
+            gap: 35px;
+        }
+        
+        ${Responsive.mobile} {
+            flex-direction: column;
+            gap: 35px;
+            margin-top: 44px;
+        }
+`;
 const ItemValue = styled.span`
-color: var(--text-color);
-font-size: 1.8rem;
-font-family: fira-go;
-font-weight: 500;
+    color: var(--text-color);
+    font-size: 1.8rem;
+    font-family: fira-go;
+    font-weight: 500;
 `;
 
 
@@ -33,6 +53,10 @@ const Price = styled.span`
     font-family: fira-go;
     font-weight: 600;
     margin-top: 8px;
+        ${Responsive.mobile}{
+            margin-top: -83px;
+            margin-left: auto;
+        }
 `;
 
 const OldPrice = styled(Price)`
@@ -46,7 +70,7 @@ const OldPrice = styled(Price)`
 const IconWrapper = styled.div`
     height: 54px;
     width: 54px;
-    background-color: rgba(224, 224, 224, .3);
+    /* background-color: rgba(224, 224, 224, .3); */
     display: flex;
     align-items: center;
     justify-content: center;
@@ -62,64 +86,10 @@ const IconWrapper = styled.div`
 `;
 
 const ItemWrapper = styled.div`
-    background-color: yellow;
-    img {
-        width: 290px;
-        height: 290px;
-        border-radius: 14px;
-        margin-right: 22px;
-    }
-    img + div {
-        background-color: red;
-            div {
-                &:nth-child(1) {// item main title
-                    background-color: aqua;
-                    font-size: 24px;
-                    margin-bottom: 15px;
-                    margin-right: 0px !important;
-                }
-                &:nth-child(2) {// start wrapper
-                    background-color: yellowgreen;
-                    margin-bottom: 0px !important;
-                        div {
-                            svg {// star
-                                height: 20px;
-                                width: 20px;
-                                margin-right: 10px !important;
-                                    &:last-child {
-                                        margin-right: 0px;
-                                    }
-                            }
-                        }
-                         span {//view count text
-                                font-size: 16px !important;
-                                margin-left: 15px;
-                                margin-top: -12px;
-                                white-space: nowrap;
-                            }
-                }
-                &:nth-child(3) {//size
-                    background-color: brown;
-                    margin-bottom: 14px !important;
-                        span {
-                            font-size: 18px;
-                        }
-                }
-                &:nth-child(4) { // color
-                    background-color: #616161;
-                    margin-bottom: 14px !important;
-                        span {
-                            font-size: 18px;
-                        }
-                }
-                &:nth-child(5) {// count
-                    background-color: #ebebeb;
-                    margin-bottom: 14px !important;
-                        span {
-                            font-size: 18px;
-                        }
-                }
-            }
+    /* background-color: yellow; */
+    
+    ${Responsive.mobile}{
+        
     }
 `;
 
@@ -131,11 +101,13 @@ const AddressTitle = styled.div`
     font-size: 18px;
     font-family: 'BPG WEB 002 CAPS';
     margin-bottom: 20px;
+    white-space: nowrap;
 `;
 const AddressItem = styled.div`
     display: flex;
     gap: 14px;
     position: relative;
+    max-width: 290px;
 `;
 const LocationIconStyle = styled(IoLocationSharp)`
     color: var(--text-color);
@@ -147,15 +119,111 @@ const PayMentMethod = styled.span`
     font-family: 'helvetica';
     opacity: 0.5;
 `;
+const DividerStyle = styled.div`
+    display: flex;
+    border-bottom: 1px solid rgba(34, 34, 34, .1);
+    margin-top: 50px;
+    margin-bottom: 30px;
+`;
+const DescriptionTitle = styled.div`
+    font-size: 17px;
+    font-family: 'helvetica';
+    font-weight: bold;
+    color: var(--text-color);
+    margin-bottom: 20px;
+`;
+const DescriptionText = styled.div`
+    font-size: 18px;
+    line-height: 27px;
+    font-family: 'helvetica';
+    color: var(--text-color);
+`;
+const AddressWrapperStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+const PayMentMethodWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+// item
+const ItemImage = styled.img`
+    object-fit: cover;
+    width: 290px;
+    height: 290px;
+    border-radius: 14px;
+    margin-right: 22px;
+        ${Responsive.mobile}{
+            width: 100%;
+            max-height: 350px;
+            margin-right: 0px;
+            margin-bottom: 34px;
+        }
+`;
+const ItemName = styled.div`
+    color: var(--text-color);
+    font-family: fira-go;
+    font-weight: 600;
+    /* background-color: aqua; */
+    font-size: 24px;
+    margin-bottom: 15px;
+    margin-right: 0px;
+`;
+const StarsWrapper = styled.div`
+    /* background-color: yellowgreen; */
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+const Stars = styled.div`
+    display: flex;
+`;
+const StartIcon = styled(BsStarFill)`
+    height: 20px;
+    width: 20px;
+    margin-right: 10px !important;
+        &:last-child {
+            margin-right: 0px;
+        }
+`;
+const ViewCount = styled(Count)`
+    font-size: 16px;
+    margin-left: 15px;
+    white-space: nowrap;
+`;
+const ItemListWrapper = styled.div`
+    /* background-color: green; */
+    margin-bottom: 14px;
+        span {
+            font-size: 18px;
+        }
+        ${Responsive.mobile} {
+            width: fit-content;
+        }
+`;
+const ItemWrapperStyle = styled.div`
+    display: flex;
+        ${Responsive.mobile}{
+            flex-direction: column;
+        }
+`;
+
+const Breadcrumbs = styled.span`
+    color: var(--text-color);
+    font-size: 16px;
+    font-family: 'helvetica';
+    opacity: 0.8;
+`;
 
 
 const History: NextPage = () => {
     return (
         <>
-            <section className={styles.section}>
+            <SectionStyle className={styles.section}>
                 <div className={styles.container}>
                     <SectionTitle>ყიდვის ისტორია</SectionTitle>
-                    <span className={styles.breadcrumbs}>მთავარი / პროდუქტები / ყიდვის ისტორია / Reima Overalls</span>
+                    <Breadcrumbs>მთავარი / პროდუქტები / ყიდვის ისტორია / Reima Overalls</Breadcrumbs>
                     <Link href={{
                         pathname: '/profile',
                         query: { tab: 'orders-history' },
@@ -167,12 +235,12 @@ const History: NextPage = () => {
                             უკან დაბრუნება
                         </div>
                     </Link>
-                    <div className={styles.wrapper}>
+                    <WrapperStyle className={styles.wrapper}>
                         <ItemWrapper>
                             <Item />
                         </ItemWrapper>
                         <BadgeStyle>დადასტურებული</BadgeStyle>
-                        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '37.0rem' }}>
+                        <AddressWrapperStyle>
 
                             <AddressTitle>მისამართი:</AddressTitle>
                             <AddressItem>
@@ -184,30 +252,29 @@ const History: NextPage = () => {
                                 </div>
                             </AddressItem>
 
-                        </div>
+                        </AddressWrapperStyle>
 
-                        <div style={{ marginRight: '14.6rem', display: 'flex', flexDirection: 'column', width: '100%', maxWidth: '28.9rem' }}>
+                        <PayMentMethodWrapper>
                             <AddressTitle>გადახდის მეთოდი</AddressTitle>
                             <PayMentMethod>უნაღდო ანგარიშწორება</PayMentMethod>
                             {/* <div className={styles.paymentMethod}>
                                 <img className={styles.paymentIcon} src={'/assets/visa.png'} />
                                 <div className={styles.cardNumber}>4332 **** **** **83  </div>
                             </div> */}
-                        </div>
-                    </div>
+                        </PayMentMethodWrapper>
+                    </WrapperStyle>
 
                 </div>
 
-                <div className={styles.divider}>
-                </div>
+                <DividerStyle />
 
-                <div className={styles.descriptionTitle}>
+                <DescriptionTitle>
                     დამატებითი ინფორმაცია
-                </div>
-                <div className={styles.descriptionText}>
+                </DescriptionTitle>
+                <DescriptionText>
                     შემთხვევითად გენერირებული ტექსტი ეხმარება დიზაინერებს და ტიპოგრაფიული ნაწარმის შემქმნელებს, რეალურთან მაქსიმალურად მიახლოებული შაბლონი წარუდგინონ შემფასებელს. ხშირადაა შემთხვევა, როდესაც დიზაინის. შემთხვევითად გენერირებული ტექსტი ეხმარება დიზაინერებს და ტიპოგრაფიული ნაწარმის შემქმნელებს, რეალურთან მაქსიმალურად მიახლოებული შაბლონი წარუდგინონ შემფასებელს. ხშირადაა შემთხვევა, როდესაც დიზაინის
-                </div>
-            </section>
+                </DescriptionText>
+            </SectionStyle>
         </>
     )
 }
@@ -217,33 +284,33 @@ export default History;
 const Item = () => {
     return (
         <>
-            <div className={styles.itemWrapper}>
-                <img src={'/assets/123123.png'} className={styles.itemImage} />
+            <ItemWrapperStyle>
+                <ItemImage src={'/assets/123123.png'} />
                 <div className={styles.itemTextContainer}>
-                    <div className={styles.itemName}>Reima Overalls  1x</div>
-                    <div style={{ display: 'flex', marginBottom: '1.6rem', alignItems: 'center' }} className={styles.child}>
-                        <div style={{ display: 'flex', marginRight: '.8rem' }} >
-                            <BsStarFill style={{ marginRight: '.8rem' }} size={'2.4rem'} color={'#22D5AE'} />
-                            <BsStarFill style={{ marginRight: '.8rem' }} size={'2.4rem'} color={'#22D5AE'} />
-                            <BsStarFill style={{ marginRight: '.8rem' }} size={'2.4rem'} color={'#22D5AE'} />
-                            <BsStarFill style={{ marginRight: '.8rem' }} size={'2.4rem'} color={'#22D5AE'} />
-                            <BsStarFill style={{ marginRight: '.4rem' }} size={'2.4rem'} color={'#22D5AE'} />
-                        </div>
-                        <Count style={{ fontSize: '1.6rem' }}>402 ნახვა</Count>
-                    </div>
-                    <div style={{ marginBottom: '.6rem' }}>
+                    <ItemName>Reima Overalls  1x</ItemName>
+                    <StarsWrapper>
+                        <Stars >
+                            <StartIcon color={'#22D5AE'} />
+                            <StartIcon color={'#22D5AE'} />
+                            <StartIcon color={'#22D5AE'} />
+                            <StartIcon color={'#22D5AE'} />
+                            <StartIcon color={'#22D5AE'} />
+                        </Stars>
+                        <ViewCount>402 ნახვა</ViewCount>
+                    </StarsWrapper>
+                    <ItemListWrapper>
                         <ItemLabel>ზომა: </ItemLabel> <ItemValue>XL</ItemValue>
-                    </div>
-                    <div style={{ marginBottom: '.6rem' }}>
+                    </ItemListWrapper>
+                    <ItemListWrapper>
                         <ItemLabel>ფერი: </ItemLabel> <ItemValue>მწვანე</ItemValue>
-                    </div>
-                    <div style={{ marginBottom: '1.6rem' }}>
+                    </ItemListWrapper>
+                    <ItemListWrapper>
                         <ItemLabel>რაოდენობა: </ItemLabel> <ItemValue>1x</ItemValue>
-                    </div>
+                    </ItemListWrapper>
                     <Price>$79.90</Price>
                     <OldPrice>$123.90</OldPrice>
                 </div>
-            </div>
+            </ItemWrapperStyle>
         </>
     )
 
