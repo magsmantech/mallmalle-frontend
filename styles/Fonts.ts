bdf6fc485971b576001cb3b8997ba68ff3ca4e18
Levan Madurashvili <55601009+maduraaa@users.noreply.github.com>
2022-06-22 15:26:22 +0400

    added font family

---
+++ styles/Fonts.ts
@@ -0,0 +1,9 @@
+const Fonts = {
+    BPGWEB002Caps: "BPG WEB 002 Caps",
+    FiraGORegular: "FiraGO-Regular",
+    FiraGOMedium: "FiraGO-Medium",
+    FiraGOBold: "FiraGO-Bold",
+    FiraGOSemiBold: "FiraGO-SemiBold"
+};
+
+export default Fonts;
